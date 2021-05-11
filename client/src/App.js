40f02adc5aefac142591ae8189048cd60abe80ae
pato tena <tenapatricio@gmail.com>
2021-05-11 00:03:56 -0500

    Updated client folder structure, created components with basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import logo from './images/logo.png';
+import useStyles from './styles';
+
+const App = () => {
+    const classes = useStyles();
+    return(
+         <Container maxWidth = "LG">
+             <AppBar className = {classes.appBar} position = "static" color = "inherit">
+                    <Typography className = {classes.heading} variant = "h2" align ="center" > Red Social Piola </Typography>
+                    <img className = {classes.image} src={logo} alt = "logo" height = "60"/>
+             </AppBar>
+             <Grow in>
+                <Container>
+                    <Grid container justify = "space-between" alignItems = "strech" spacing ={3}>
+                        <Grid item xs = {12} sm = {7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs = {12} sm = {4}>  
+                            <Form / >
+                        </Grid>
+                    </Grid>
+                </Container>
+             </Grow>
+         </Container>
+           
+    );
 }
 
 export default App;
