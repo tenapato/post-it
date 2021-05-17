1225396323fead55c0186feed6ef2a4f7eb8fb8f
tenapato <tenapatricio@gmail.com>
2021-05-16 22:06:04 -0700

    Added route and controller for like button

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 // Sets a route for every post
 
 import express from 'express';
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
 
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get('/', getPosts);
 router.post('/', createPost); 
 router.patch('/:id', updatePost);  //updating posts
 router.delete('/:id', deletePost); //deleting posts
+router.patch('/:id/likePost', likePost);
 
 export default router;
