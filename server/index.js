1e0c55ae1e1ed40f7daa989f853185109c7bc0c8
tenapato <tenapatricio@gmail.com>
2021-05-19 00:16:52 -0700

    Implemented eviromental variables

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from  './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));  //set a limit for file size
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -15,10 +15,10 @@ app.use(cors());
 app.use('/posts', postRoutes);  //Every route start with posts
 //Connect to MongoDB Atlas
 
-const CONNECTION_URL = "mongodb+srv://proyectofinal_123:proyectofinal_123@cluster0.ofrja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error)=> console.log(error.message));
 
