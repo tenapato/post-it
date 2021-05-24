e891364bda4b97dffd7aaaaa48f13f62cdc5d35b
tenapato <tenapatricio@gmail.com>
2021-05-23 17:42:42 -0700

    Refactored and changed liked functionally that allows only 1 like per user

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -56,12 +56,26 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
+
+    if(!req.userId) return res.json({message: 'Unauthenticated'}); //No user is logged in
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).send('No post with that id');
     }
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true});
+
+    const index = post.likes.findIndex((id) => id === String(req.userId)); //Check if that person by id already liked that post
+
+    if(index === -1) { //If user has not liked the post
+        //Like the post
+        post.likes.push(req.userId);
+    } else {
+        // Dislike the post
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true});
 
     res.json(updatedPost);
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,15 +2,15 @@
 
 import express from 'express';
 import { getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js';
-
+import auth from '../middleware/auth.js'; //Authentication middleware
 
 const router = express.Router(); //Router instance
 
 //localhost:5000/posts 
 router.get('/', getPosts);
-router.post('/', createPost); 
-router.patch('/:id', updatePost);  //updating posts
-router.delete('/:id', deletePost); //deleting posts
-router.patch('/:id/likePost', likePost);
+router.post('/', auth, createPost); 
+router.patch('/:id', auth, updatePost);  //updating posts
+router.delete('/:id', auth, deletePost); //deleting posts
+router.patch('/:id/likePost', auth, likePost); //liking posts
 
 export default router;
